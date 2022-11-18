98d1e3b2054bb676157fb5d6f390860645d20e75
hasnatsajid <hasnatsajid@965gmail.coom>
2022-11-18 23:06:23 +0500

    added server

---
+++ controllers/user.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+const secret = 'tinysecret';
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (!oldUser) return res.status(404).json({ message: 'User does not exist' });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials!' });
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: '1h' });
+
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
+const signup = async (req, res) => {
+  const { firstName, lastName, email, password, role } = req.body;
+
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) return res.status(400).json({ message: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`, role });
+
+    const token = jwt.sign({ email: result.email, id: result._id }, secret, { expiresIn: '1h' });
+
+    res.status(200).json({ result, token });
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
+module.exports = { signin, signup };
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+const userRoutes = require('./routes/user.js');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/user', userRoutes);
+
+app.listen(PORT, async () => {
+  try {
+    mongoose.connect(`${MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log(`Server started on ${PORT}...`);
+  } catch (err) {
+    console.log(`Server failed starting ${err}...`);
+  }
+});
