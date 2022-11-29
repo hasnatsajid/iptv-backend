8a89d2a799d0bb4c36418d382eafd27fcbbf41b6
hasnatsajid <hasnatsajid@965gmail.coom>
2022-11-29 12:46:35 +0500

    added contact

---
+++ controllers/mailer.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: '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',
+    },
+  })
+);
+
+const sendEmail = (req, res, next) => {
+  const { name, subject, email, message } = req.body;
+
+  transporter
+    .sendMail({
+      to: email,
+      from: 'kobi.iptv@gmail.com',
+      subject,
+      html: message,
+    })
+    .then((result) => {
+      return res.status(200).json({ status: true, message: 'Message sent successfully :)' });
+    })
+    .catch((e) => {
+      console.log(e);
+      return res.status(500).json({ status: false, message: 'Message sending failed ;(' });
+    });
+};
+
+module.exports = sendEmail;
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ dotenv.config();
 const userRoutes = require('./routes/user.js');
 const paymentRoutes = require('./routes/payment.js');
 
+const mailer = require('./controllers/mailer.js');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URI;
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/user', userRoutes);
+app.use('/sendmail', mailer);
 app.use('/payment', paymentRoutes);
 
 app.listen(PORT, async () => {
